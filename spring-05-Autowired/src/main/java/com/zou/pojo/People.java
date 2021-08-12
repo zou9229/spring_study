357b040c5ebd84285520cd6f54292e6a42d1952e
zou <923816483@qq.com>
2021-08-12 23:23:16 +0800

    注解方式

--- spring-05-Autowired/src/main/java/com/zou/pojo/People.java
+++ spring-05-Autowired/src/main/java/com/zou/pojo/People.java
@@ -1,32 +1,35 @@
 package com.zou.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import javax.annotation.Resource;
+
 public class People {
+
+    //如果显示定义了Autowired的required属性为false，说明这个对象可以为null，否则不允许为空
+//    @Autowired(required = false)
+    @Resource(name = "cat2")
     private Cat cat;
+//    @Autowired
+//    @Qualifier(value = "dog222")
+    @Resource
     private Dog dog;
+
     private String name;
+
+
 
     public Cat getCat() {
         return cat;
-    }
-
-    public void setCat(Cat cat) {
-        this.cat = cat;
     }
 
     public Dog getDog() {
         return dog;
     }
 
-    public void setDog(Dog dog) {
-        this.dog = dog;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override
