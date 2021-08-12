732de902ec9f7a657449bcc26451c2c46e6a2945
zou <923816483@qq.com>
2021-08-12 23:23:16 +0800

    不需要xml方式

--- spring-07-appconfig/src/main/java/com/zou/config/ZouConfig.java
+++ spring-07-appconfig/src/main/java/com/zou/config/ZouConfig.java
@@ -4,6 +4,9 @@ import com.zou.pojo.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//这个也会Spring容器托管，注册到容器中，因为他本来就是一个@Component
+// @Configuration代表这是一个配置类，就和我们之前看的beans.xml一样的
+
 @Configuration
 public class ZouConfig {
 
